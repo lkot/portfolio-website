b7e99cd2b6f6f228381a38d0b61b0ae31dadd3d6
Lukasz Kot <lukasz.kot@hotmail.com>
2021-04-13 10:42:18 +0200

    Portfolio component created & styled.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../logo.png";
+import logo from "../logo-lk.png";
 // REACT FONTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-black">
       <div className="container">
         <a className="navbar-brand" href="#">
           <img className="logo" src={logo} alt="logo..." />
